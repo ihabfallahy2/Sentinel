1307ac3d52ef4d415f801189cce4393ebb12e7d4
Ihab Fallahy <ihab2000@hotmail.es>
2024-03-10 05:16:16 +0100

    feat: Initial app

---
+++ src/test/java/com/itenfa/batsentinel/BatSentinelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itenfa.batsentinel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatSentinelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
